1e4d5a0a8a1e62713d5f1ff21ac76bf2782c7fcd
Rainel Dave R. Azures <raineldaveazures0929@gmail.com>
2025-01-23 09:02:14 +0800

    Add files via upload

---
+++ Azures.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num[10];
+	
+	for(int i=0; i<10; i++){
+		cin >> num[i];
+	}
+	
+	cout << num << endl;
+	for(int j=9; j>=0; j--){
+		cout << num [j] << endl;
+	}
+	
+	return 0;
+}
